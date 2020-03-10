c64152fd78b57e8aab0b188b84a9e8c0d4365b37
Manuel H <haugg10@gmail.com>
2020-03-10 17:45:31 +0100

    new action menu

--- static/tableeditor.js
+++ static/tableeditor.js
@@ -20,4 +20,39 @@ function showSaveBtn(){
     document.getElementById("saveBtns").style.visibility = "visible";
 }
 
+function actionmenu_call(targetElm,rowId){
+    var actAry = ["edit","delete","copy"];
+    var targetUrl = "/row/"
+    clear_elements_by_class("menuBox",buildBtns);
+    
+    function  buildBtns(){ 
+        var menuBox = document.createElement("div");
+        menuBox.classList.add("menuBox");
+        targetElm.appendChild(menuBox);
+
+        for(var prop in actAry){
+            var menuBtn = document.createElement("div");
+            menuBtn.innerHTML = actAry[prop];
+            menuBtn.onclick = function(){
+                clear_elements_by_class("menuBox");
+                location.href=targetUrl+this.innerHTML+"/"+rowId;
+            };
+            menuBox.appendChild(menuBtn);
+        }
+    }
+
+
+}
+
+function clear_elements_by_class(clName,callBack=false){
+    var elmAry = document.getElementsByClassName(clName);
+
+    for (var i = 0; i < elmAry.length; i++) {
+        elmAry[i].parentNode.removeChild(elmAry[i]);
+    }
+    if(callBack!=false){
+        callBack();
+    }
+}
+
 
