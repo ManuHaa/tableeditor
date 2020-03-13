db853d3267844381ea955af73f92112d5c870162
Manuel H <haugg10@gmail.com>
2020-03-13 17:04:51 +0100

    form function added

--- static/tableeditor.js
+++ static/tableeditor.js
@@ -34,7 +34,7 @@ function (event){
 
 // Aufruf des Drop-Down Menüs
 function actionmenu_call(targetElm,rowId){      // Zielelement und ZeilenId werden als Parameter übergeben
-    var actAry = ["edit","delete","copy","add"];      // alle möglichen Aktionen werden im actAry gespeichert
+    var actAry = ["edit","delete","copy"];      // alle möglichen Aktionen werden im actAry gespeichert
     var targetUrl = "/row/"                     // Ziel Url wird definiert
     clear_elements_by_class("menuBox",buildBtns);                   // Funktion zum einklappen des Menüs
     
@@ -127,10 +127,25 @@ function addRow(){
 }
 
 function editRow(){
-    var editTarget = document.getElementById("stdTable");
+    var editTarget = document.getElementById("tr");
     if(onclick==true){
         editTarget.contentEditable='true';
     }
+
+    let data = editTarget.value;
+    const contentToExport = new Blob([data]);
+    const fileName = 'Data.txt';
+    let Link = document.createElement('a');
+    Link.download = fileName;
+
+    if(window.webkitURL != null){
+        Link.href = window.webkitURL.createObjectURL(contentToExport);
+    } else{
+        Link.href = window.webkitURL.createObjectURL(contentToExport);
+        Link.style.display='none';
+        document.body.appendChild(Link);
+    }
+    Link.click();
 }
 
 function cloneRow(){
@@ -144,6 +159,57 @@ function cloneRow(){
    addTarget.appendChild(newRow);
     rowsNumber++;
     }
+
+
 }
 
+// -------------------------------------
 
+function editRowTestTable(no){
+    document.getElementById('editButton').style.display='block';
+    document.getElementById('saveButton').style.display='block';
+
+    var name = document.getElementById('nr'+no);
+    var country = document.getElementById('cr'+no);
+    var age = document.getElementById('ar'+no);
+
+    var name_data = name.innerHTML;
+    var country_data = country.innerHTML;
+    var age_data = age.innerHTML;
+
+    name.innerHTML="<input type='text' id='name_text"+no+"' value='"+name_data+"'>";
+    country.innerHTML="<input type='text' id='country_text"+no+"' value='"+country_data+"'>";
+    age.innerHTML="<input type='text' id='age_text"+no+"' value='"+age_data+"'>";
+}
+
+function saveRowTestTable(no){
+    var nameVal = document.getElementById('name_text'+no).value;
+    var countryVal = document.getElementById('country_text'+no).value;
+    var ageVal = document.getElementById('age_text'+no).value;
+
+    document.getElementById("nr"+no).innerHTML=nameVal;
+    document.getElementById("cr"+no).innerHTML=countryVal;
+    document.getElementById("ar"+no).innerHTML=ageVal;
+
+    document.getElementById("editButton"+no).style.display="block";
+    document.getElementById("saveButton"+no).style.display="none";
+}
+
+function deleteRowTestTable(no){
+    document.getElementById('row'+no).outerHTML="";
+}
+
+function addRowTestTable(){
+    var newName = document.getElementById('new_name').value;
+    var newCountry = document.getElementById('new_country').value;
+    var newAge = document.getElementById('new_age').value;
+
+    var testTable = document.getElementById('datatable');
+    var testTableLen = (testTable.rows.length)-1;
+    var row = testTable.insertRow(testTableLen).outerHTML="<tr id='row"+testTableLen+"'><td id='name_row"+testTableLen+"'>"+newName+"</td><td id='country_row"+testTableLen+"'>"+newCountry+"</td><td id='age_row"+testTableLen+"'>"+newAge+"</td><td><input type='button' id='edit_button"+testTableLen+"' value='Edit' class='edit' onclick='editRowTestTable("+testTableLen+")'> <input type='button' id='save_button"+testTableLen+"' value='Save' class='save' onclick='saveRowTestTable("+testTableLen+")'> <input type='button' value='Delete' class='delete' onclick='deleteRowTestTable("+testTableLen+")'></td></tr>";
+
+    document.getElementById('newName').value="";
+    document.getElementById('newCountry').value="";
+    document.getElementById('newAge').value="";
+
+}
--- tableeditor.py
+++ tableeditor.py
@@ -4,25 +4,30 @@ import json
 import time
 import os
 
-from flask import Flask, render_template, send_from_directory,request
+from flask import Flask, render_template, send_from_directory,request,redirect
 app = Flask(__name__, static_url_path='/static')
 app.debug = True
+
+
+
+#-Global variables and definitions-----------------------------
+tableName = 'contacts'
+jsonObj = open('conf/'+tableName+'.json')    # ein File für conf/contacts.json wird geöffnet und in jsonObj gespeichert
+jsonStr = jsonObj.read()                # jsonObj wird eingelesen und in jsonStr gespeichert
+tableDef = json.loads(jsonStr)          # der jsonStr wird in in das JSON Format umgewandelt und in tableDef gespeichert
+    
+jsonObj = open('data/'+tableName+'.json')    # das gleiche passiert für für data/contacts.json
+jsonStr = jsonObj.read()
+tableData = json.loads(jsonStr)
 
 #-App Routing-------------------------------------------------
 @app.route('/')
 def html_helloworld():
     return render_template('helloworld.html')
 
-@app.route('/table')
+@app.route('/table' )
 def html_table():
-    jsonObj = open('conf/contacts.json')    # ein File für conf/contacts.json wird geöffnet und in jsonObj gespeichert
-    jsonStr = jsonObj.read()                # jsonObj wird eingelesen und in jsonStr gespeichert
-    tableDef = json.loads(jsonStr)          # der jsonStr wird in in das JSON Format umgewandelt und in tableDef gespeichert
     
-    jsonObj = open('data/contacts.json')    # das gleiche passiert für für data/contacts.json
-    jsonStr = jsonObj.read()
-    tableData = json.loads(jsonStr)
-
     return render_template(                 # unter der route /table wird table.html gerendert mit den von oben übergebenen tableDef und tableData
         'table.html',
         tableDef=tableDef,
@@ -31,29 +36,33 @@ def html_table():
 
 @app.route('/row/edit/<rowId>')
 def html_edit(rowId):
-    jsonObj = open('conf/contacts.json')    # ein File für conf/contacts.json wird geöffnet und in jsonObj gespeichert
-    jsonStr = jsonObj.read()                # jsonObj wird eingelesen und in jsonStr gespeichert
-    tableDef = json.loads(jsonStr)          # der jsonStr wird in in das JSON Format umgewandelt und in tableDef gespeichert
-    
-    jsonObj = open('data/contacts.json')    # das gleiche passiert für für data/contacts.json
-    jsonStr = jsonObj.read()
-    tableData = json.loads(jsonStr)
-
+    rowId = int(rowId)
+    #print(tableData[rowId])
+    try: 
+        rowData = tableData[rowId]
+    except: 
+        return 'row not found', 404
     return render_template(                 # unter der route /table wird table.html gerendert mit den von oben übergebenen tableDef und tableData
         'edit.html',
         tableDef=tableDef,
-        tableData=tableData
+        rowData=rowData,
+        rowId = rowId
     )
+@app.route('/api/row/edit/<rowId>', methods=["POST"])  
+def api_edit(rowId):
+    rowId = int(rowId)
+    dataIn = request.form
+    newTableData = tableData
+    newTableData[rowId] = dataIn 
+    jsonOut = json.dumps(newTableData)
+    jsonFile = open('data/'+tableName+'.json','w')    # das gleiche passiert für für data/contacts.json
+    jsonFile.write(jsonOut)
+
+    return redirect('/table',code=302)
 
 @app.route('/row/delete/<rowId>')
 def html_delete(rowId):
-    jsonObj = open('conf/contacts.json')    # ein File für conf/contacts.json wird geöffnet und in jsonObj gespeichert
-    jsonStr = jsonObj.read()                # jsonObj wird eingelesen und in jsonStr gespeichert
-    tableDef = json.loads(jsonStr)          # der jsonStr wird in in das JSON Format umgewandelt und in tableDef gespeichert
-    
-    jsonObj = open('data/contacts.json')    # das gleiche passiert für für data/contacts.json
-    jsonStr = jsonObj.read()
-    tableData = json.loads(jsonStr)
+
 
     return render_template(                 # unter der route /table wird table.html gerendert mit den von oben übergebenen tableDef und tableData
         'delete.html',
@@ -64,53 +73,46 @@ def html_delete(rowId):
 @app.route('/row/copy/<rowId>')
 def html_copy(rowId):
     print(rowId)
-    jsonObj = open('conf/contacts.json')    # ein File für conf/contacts.json wird geöffnet und in jsonObj gespeichert
-    jsonStr = jsonObj.read()                # jsonObj wird eingelesen und in jsonStr gespeichert
-    tableDef = json.loads(jsonStr)          # der jsonStr wird in in das JSON Format umgewandelt und in tableDef gespeichert
     
-    jsonObj = open('data/contacts.json')    # das gleiche passiert für für data/contacts.json
-    jsonStr = jsonObj.read()
-    tableData = json.loads(jsonStr)
-
     return render_template(                 # unter der route /table wird table.html gerendert mit den von oben übergebenen tableDef und tableData
         'copy.html',
         tableDef=tableDef,
         tableData=tableData
     )
 
-@app.route('/dropdowntester')
-def html_dropdowntester():
-    jsonObj = open('conf/contacts.json')    # ein File für conf/contacts.json wird geöffnet und in jsonObj gespeichert
-    jsonStr = jsonObj.read()                # jsonObj wird eingelesen und in jsonStr gespeichert
-    tableDef = json.loads(jsonStr)          # der jsonStr wird in in das JSON Format umgewandelt und in tableDef gespeichert
+#@app.route('/dropdowntester')
+#def html_dropdowntester():
+#    jsonObj = open('conf/contacts.json')    # ein File für conf/contacts.json wird geöffnet und in jsonObj gespeichert
+#    jsonStr = jsonObj.read()                # jsonObj wird eingelesen und in jsonStr gespeichert
+#    tableDef = json.loads(jsonStr)          # der jsonStr wird in in das JSON Format umgewandelt und in tableDef gespeichert
     
-    jsonObj = open('data/contacts.json')    # das gleiche passiert für für data/contacts.json
-    jsonStr = jsonObj.read()
-    tableData = json.loads(jsonStr)
+#    jsonObj = open('data/contacts.json')    # das gleiche passiert für für data/contacts.json
+#    jsonStr = jsonObj.read()
+#    tableData = json.loads(jsonStr)
 
-    return render_template(
-        'dropdowntester.html',
-        tableDef=tableDef,
-        tableData=tableData
-        )
+#    return render_template(
+#        'dropdowntester.html',
+#        tableDef=tableDef,
+#        tableData=tableData
+#        )
         
 
-@app.route('/row/add/<rowId>')
-def html_add(rowId):
-    print(rowId)
-    jsonObj = open('conf/contacts.json')    # ein File für conf/contacts.json wird geöffnet und in jsonObj gespeichert
-    jsonStr = jsonObj.read()                # jsonObj wird eingelesen und in jsonStr gespeichert
-    tableDef = json.loads(jsonStr)          # der jsonStr wird in in das JSON Format umgewandelt und in tableDef gespeichert
-    
-    jsonObj = open('data/contacts.json')    # das gleiche passiert für für data/contacts.json
-    jsonStr = jsonObj.read()
-    tableData = json.loads(jsonStr)
-
-    return render_template(                 # unter der route /table wird table.html gerendert mit den von oben übergebenen tableDef und tableData
-        'add.html',
-        tableDef=tableDef,
-        tableData=tableData
-    )
+#@app.route('/row/add/<rowId>')
+#def html_add(rowId):
+#    print(rowId)
+#    jsonObj = open('conf/contacts.json')    # ein File für conf/contacts.json wird geöffnet und in jsonObj gespeichert
+ #   jsonStr = jsonObj.read()                # jsonObj wird eingelesen und in jsonStr gespeichert
+#    tableDef = json.loads(jsonStr)          # der jsonStr wird in in das JSON Format umgewandelt und in tableDef gespeichert
+#    
+#    jsonObj = open('data/contacts.json')    # das gleiche passiert für für data/contacts.json
+#    jsonStr = jsonObj.read()
+#    tableData = json.loads(jsonStr)
+#
+#    return render_template(                 # unter der route /table wird table.html gerendert mit den von oben übergebenen tableDef und tableData
+#        'add.html',
+#        tableDef=tableDef,
+#        tableData=tableData
+#    )
 
 #-Application runner--------------------------------------------
 if __name__ == '__main__':
